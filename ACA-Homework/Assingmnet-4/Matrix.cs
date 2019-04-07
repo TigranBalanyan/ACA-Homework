ccb69af419cb31c4b2336106a73556872c046a06
Tigran Balanyan <tbalanyan@gmail.com>
2019-04-06 22:39:16 -0700

    Created a Matrix class.

---
+++ ACA-Homework/Assingmnet-4/Matrix.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACA_Homework.Assingmnet_4
+{
+    public class Matrix
+    {
+
+        public int[,] ArrayContent { get; set; }
+
+        public int Rows { get; set; }
+
+        public int Columns { get; set; }
+
+        public Matrix(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+
+            Random rnd = new Random();
+
+            ArrayContent = new int[Rows,Columns];
+
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    ArrayContent[i,j] = rnd.Next(10, 89);
+                }
+            }
+        }
+
+        public Matrix Inverse()
+        {
+            Matrix inverseMatrix = new Matrix(rows:Columns, columns:Rows);
+
+            return inverseMatrix;
+        }
+
+        //public Matrix Transpose()
+        //{
+        //    return new Matrix();
+        //}
+
+        public bool IsOrthogonal()
+        {
+            return true;
+        }
+
+        static int SmallestNumber(Matrix matrix)
+        {
+            return 15;
+        }
+
+        static int BiggestNumber(Matrix matrix)
+        {
+            return 15;
+        }
+
+    }
+}
--- ACA-Homework/Program.cs
+++ ACA-Homework/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ACA_Homework.Assingment_3;
+using ACA_Homework.Assingmnet_4;
 
 namespace ACA_Homework
 {
@@ -11,143 +7,13 @@ namespace ACA_Homework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the size of an array that you want to sort");
-            int sizeOfAnArray = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please, enter the number of Rows!");
+            int rows = Convert.ToInt32(Console.ReadLine());
 
-            int[] randomArray = new int[sizeOfAnArray];
-            randomArray = ArrayRandomiser(sizeOfAnArray, 10, 100);
+            Console.WriteLine("Please, enter the number of Columns!");
+            int columns = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Select which algorithm you want to perform:\n" +
-                              "1. Insertion sort\n" +
-                              "2. Bubble sort\n" +
-                              "3. Quick sort\n" +
-                              "4. Heap sort\n" +
-                              "5. Merge sort\n" +
-                              "6.All");
-
-            string algorithmNumber = Console.ReadLine();
-
-            List<int> numbersOfAlgorithms = AlgorithmNumbers(algorithmNumber);
-
-            if (numbersOfAlgorithms != null)
-            {
-                var Algorithms = new List<Algorithm>();
-
-                foreach (var i in numbersOfAlgorithms)
-                {
-                    switch (i)
-                    {
-                        case 1: 
-                            Algorithms.Add(new InsertionSort());
-                            break;
-                        case 2:
-                            Algorithms.Add(new BubbleSort());
-                            break;
-                        case 3:
-                            Algorithms.Add(new QuickSort()); // not implemented
-                            break;
-                        case 4:
-                            Algorithms.Add(new HeapSort());
-                            break;
-                        case 5:
-                            Algorithms.Add(new MergeSort()); // not implemented
-                            break;
-                        default:
-                            Console.WriteLine("Please, enter the valid numbers!");
-                            break;
-                    }
-                }
-
-                foreach (var algorithm in Algorithms)   
-                {
-                    Console.WriteLine(Convert.ToString(algorithm.ToString()));
-                    Console.WriteLine(algorithm.RunningTime(randomArray));
-                    Console.WriteLine(algorithm.UsedMemory());
-                    Console.WriteLine();
-                }
-            }
-            else
-            {
-                Console.WriteLine("Please, enter the valid numbers!");
-            }
-        }
-
-        private static List<int> AlgorithmNumbers(string algorithmNumber)
-        {
-            try
-            {
-                if (algorithmNumber.Length == 1 || algorithmNumber.Length == 3)
-                {
-                    List<int> numberOfAlgorithms = new List<int>();
-
-                    if (algorithmNumber.Contains('6'))
-                    {
-                        numberOfAlgorithms.Add(1);
-                        numberOfAlgorithms.Add(2);
-                        numberOfAlgorithms.Add(3);
-                        numberOfAlgorithms.Add(4);
-                        numberOfAlgorithms.Add(5);
-
-                        return numberOfAlgorithms;
-                    }
-
-                    if (algorithmNumber.Length == 1)
-                    {
-                        numberOfAlgorithms.Add(Convert.ToInt32(algorithmNumber[0]) - 48);
-                        return numberOfAlgorithms;
-                    }
-                    else
-                    {
-                        int firstNumber = Convert.ToInt32(algorithmNumber[0]) - 48;
-                        int lastNumber = Convert.ToInt32(algorithmNumber[2]) - 48;
-
-                        if(firstNumber > 5 || lastNumber > 5)
-                        {
-                            return null;
-                        }
-
-                        if (algorithmNumber[1] == ',')
-                        {
-                            numberOfAlgorithms.Add(firstNumber);
-                            numberOfAlgorithms.Add(lastNumber);
-
-                            return numberOfAlgorithms;
-                        }
-                        if (algorithmNumber[1] == '-')
-                        {
-                            for (int i = firstNumber; i <= lastNumber; i++)
-                            {
-                                numberOfAlgorithms.Add(i);
-                                Console.WriteLine(i);
-                            }
-
-                            return numberOfAlgorithms;
-                        }
-                    }
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Source + "Format is invalid");
-            }
-
-            return null;
-        }
-
-        private static int[] ArrayRandomiser(int sizeOfAnArray, int Min, int Max)
-        {
-            int[] randomArray = new int[sizeOfAnArray];
-            Random randNum = new Random();
-            for (int i = 0; i < sizeOfAnArray; i++)
-            {
-                randomArray[i] = randNum.Next(Min, Max);
-            }       
-            
-            return randomArray;
+            Matrix matrix = new Matrix(rows: rows, columns: columns);
         }
     }
 }

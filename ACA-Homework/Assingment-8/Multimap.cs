cd8cd26901398c6d90924cf25907275b0c147497
Tigran Balanyan <tbalanyan@gmail.com>
2019-04-30 16:55:40 -0700

    Work is approximatly done, needs testing and 2 more methods.

---
+++ ACA-Homework/Assingment-8/Multimap.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACA_Homework.Assingment_7
+{
+
+    /// <summary>
+    /// A MultiMap generic collection class that can store more than one value for a Key.
+    /// </summary>
+    /// <typeparam name="Key"></typeparam>
+    /// <typeparam name="Value"></typeparam>
+    public class MultiMap<TKey, TValue> : IDictionary<TKey, TValue>
+    {
+
+        public TValue this[TKey key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ICollection<TKey> Keys { get; set; }
+
+        public ICollection<TValue> Values { get; set; }
+
+        public Dictionary<TKey, List<TValue>> MultiMapContent { get; set; }
+
+        public int Count { get; set; }
+
+        public bool IsReadOnly { get; set; }
+
+        public MultiMap()
+        {
+            MultiMapContent = new Dictionary<TKey, List<TValue>>();
+            Keys = MultiMapContent.Keys;
+       //     Values = MultiMapContent.Values;
+        }
+
+        /// <summary>
+        /// Adds an element to the MultiMap.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Add(TKey key, TValue value)
+        {
+            List<TValue> listItems = null;
+            MultiMapContent.TryGetValue(key, out listItems);
+            MultiMapContent.Add(key, listItems);
+        }
+
+        /// <summary>
+        /// Adds an key value pair to the MultiMap.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            List<TValue> listItems = null;
+            MultiMapContent.TryGetValue(item.Key, out listItems);
+            listItems.Add(item.Value);
+            MultiMapContent.Add(item.Key, listItems);
+        }
+
+        public void Clear()
+        {
+            MultiMapContent.Clear();
+        }
+
+        /// <summary>
+        /// Checks if MultiMap contains given item.
+        /// </summary>
+        /// <param name="item"></param>
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            List<TValue> listItems = null;
+            MultiMapContent.TryGetValue(item.Key, out listItems);
+            if (listItems.Contains(item.Value))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Checks if MultiMap contains given key.
+        /// </summary>
+        /// <param name="key"></param>
+        public bool ContainsKey(TKey key)
+        {
+            foreach(var Keys in MultiMapContent.Keys)
+            {
+                if(Keys.Equals(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }   
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(TKey key)
+        {
+            List<TValue> listItems = null;
+            if(MultiMapContent.TryGetValue(key, out listItems))
+            {
+                MultiMapContent.Remove(key);
+                return true;
+            }
+            return false;        
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            List<TValue> listItems = null;
+            if (MultiMapContent.TryGetValue(item.Key, out listItems))
+            {
+                MultiMapContent.Remove(item.Key);
+                listItems.Remove(item.Value);
+                return true;
+            }
+            return false;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            if (MultiMapContent.Keys.Contains(key))
+            {
+                List<TValue> listItems = null;
+                MultiMapContent.TryGetValue(key, out listItems);
+                value = listItems[0];
+                return true;
+            }
+            else
+            {
+                value = default(TValue);
+                return false;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ACA-Homework/Program.cs
+++ ACA-Homework/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using ACA_Homework.Assingment_6;
 using ACA_Homework.Assingment_6.Exercise_4;
+using ACA_Homework.Assingment_7;
 using static ACA_Homework.Assingment_6.Airport;
 
 namespace ACA_Homework
@@ -10,111 +11,12 @@ namespace ACA_Homework
     {
         static void Main(string[] args)
         {
-            /*
-            #region Exercise-1
-
-            Console.WriteLine("----------------------------Exercise - 1---------------------------- ");
-
-            OnlineAttendance attendance = new OnlineAttendance(); //creating an Online Attendance class to handle everething
-            attendance.OnBannedNames += AlarmTheBannedName; //subscribint to an event
-            attendance.Attend();
-
-            #endregion
-            */
-
-            /*
-            #region Exercise-2
-
-            Console.WriteLine("----------------------------Exercise - 2---------------------------- ");
-
-            Console.WriteLine("Please enter your text");
-
-            string text = Console.ReadLine(); //taking a text
-
-            Abbreviation abb = new Abbreviation(text); //giving text to a class
-            Console.WriteLine(abb.Abbreviate()); //abrreviating a text and outputing it
-
-            #endregion
-            */
-
-            /*
-            #region Exercise-3
-
-            List<Airport> airports = new List<Airport>(); //creating a list of airports
-            airports.Add(new Airport(Size.Large));      // I am too lazy to give a name to a airport, 
-            airports.Add(new Airport(Size.Medium));      //  so I created a constructor that takes 1 parameter 
-            airports.Add(new Airport(Size.SuperMega));   //and created several objects to test my programm
-            airports.Add(new Airport(Size.Small));
-            airports.Add(new Airport(Size.Small));
-            airports.Add(new Airport(Size.Medium));
-            airports.Add(new Airport(Size.Mega));
-            airports.Add(new Airport(Size.Large));
-            airports.Add(new Airport(Size.Small));
-            airports.Add(new Airport(Size.Large));
-            airports.Add(new Airport(Size.SuperMega));
-                                                       
-
-            airports.Sort(new AirportComparer());
-
-            foreach(var airport in airports) //prints the airports in descending order
+            var multi = new MultiMap<int, string>();
+            multi.Add(5, "Hello");
+            foreach(var m in multi.Keys)
             {
-                Console.WriteLine(airport.SizeOfTheAirport);
             }
 
-            #endregion
-            */
-
-            #region Exercise-4
-
-            StudentCatalog students = new StudentCatalog(); //Creating a student catalog
-
-            Student anna = new Student(12, "Anna"); 
-            Student betty = new Student(338, "Betty");
-            Student carl = new Student(92, "Carl");
-            Student diana = new Student(295, "Diana");
-
-            anna.AddTestResult("English", 85);
-            anna.AddTestResult("Math", 70);
-            anna.AddTestResult("Biology", 90);
-            anna.AddTestResult("French", 52);
-
-            betty.AddTestResult("English", 77);
-            betty.AddTestResult("Math", 82);
-            betty.AddTestResult("Chemistry", 65);
-            betty.AddTestResult("French", 41);
-
-            carl.AddTestResult("English", 55);
-            carl.AddTestResult("Math", 48);
-            carl.AddTestResult("Biology", 70);
-            carl.AddTestResult("French", 38);
-
-            students.AddStudent(anna);
-            students.AddStudent(betty);
-            students.AddStudent(carl);
-            students.AddStudent(diana);
-
-            Console.WriteLine(students.GetStudent(12).Name); //Getting student by Id
-
-            Console.WriteLine(students.GetAverageForStudent(338)); //Getting the average score for a student by id
-
-            Console.WriteLine(students.GetTotalAverage()); //Getting the total average of a student's score
-
-
-            foreach(var bestStudent in students.GetTopThreeStudents())
-            {
-                Console.WriteLine(bestStudent.Name + " - average score is " + students.GetAverageForStudent(bestStudent.Id)); //printing the names of top students and their scores
-            }
-
-            #endregion
-        }
-
-        /// <summary>
-        /// Spy alarm, in case spy event is raised, and spy entered our site
-        /// </summary>
-        /// <param name="name">The super secret name of a spy</param>
-        private static void AlarmTheBannedName(string name)
-        {
-            Console.WriteLine($"Achtung the spy {name} entered this site!");
         }
     }
 }
